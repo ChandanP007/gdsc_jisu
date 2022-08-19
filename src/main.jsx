885ca8bee7e9e28a0b658dcf8a4436036364f63b
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-08-19 21:11:39 +0530

    Routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// File imports
+import { Team , Events, Contact,Timeline,Projects , NotFound} from './routes'
 import App from './App'
 import './index.css'
 
+import { BrowserRouter,Routes,  Route, } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/timeline" element={<Timeline />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:id" element={<Projects />} />
+      <Route path="/:id" element={<NotFound />} />
+    </Routes>
+
+  </BrowserRouter>
 )
