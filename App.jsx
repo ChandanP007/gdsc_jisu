be047465c7878ce1abea13c1c6d7a7d3637ebb5b
Chandan Pandey <chandan.dv682@gmail.com>
2022-08-21 20:49:38 +0530

    Hero Section Done, Will modify later
    -added Hero component in App.jsx
    -created a images folder for the image assets
    -created Hero Hero.jsx iinside Components
    -added some css in App.css

--- App.jsx
+++ App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+    <>
     <div className="App">
       <Navbar />
+      <Hero />
+      <Header />
+      <Footer />
+
     </div>
+
+    </>
   )
 }
 
