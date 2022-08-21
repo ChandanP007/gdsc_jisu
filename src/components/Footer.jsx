1d1c5969895a1a73cfa8ae812168aef14fba4970
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-08-21 18:34:15 +0530

    footer init

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className=' bg-white dark:bg-black'>
+      <div className='flex justify-around  gap-4 text-white h-[400px] sticky bottom-0 p-10'>
+        <div>Google </div>
+        <div>developer</div>
+        <div>student </div>
+        <div>club</div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
