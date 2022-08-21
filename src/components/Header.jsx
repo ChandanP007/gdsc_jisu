0ed7a30822cdb3bc331e34a4c38bab87b5d8a49d
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-08-21 18:34:22 +0530

    header init

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className=' mx-auto bg'>
+        <div className="container   m-5 p-10 mx-auto">
+            <div>
+                <h1 className='text-5xl font-bold  text-black '>
+                    Google Developer Student Club
+                </h1>
+            </div>
+
+            {/* Side logo here */}
+            <div className=''>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
