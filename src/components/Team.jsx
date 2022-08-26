26626439ec618fd89a53d89e975065ca9a0907bb
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-08-26 22:13:22 +0530

    Team Improvement

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -23,13 +23,16 @@ function Team() {
     
 
     return (
-        <div className=" pt-20 text-center mx-auto dark:bg-bg-dark">
+        <div className=" pt-20 text-center mx-auto dark:bg-slate-800">
             <h1 className='lg:text-6xl text-2xl dark:text-white font-mono'>Team</h1>
             <p className='dark:text-white font-extralight font-mono pt-5'>
                 We are a team of passionate developers who are passionate about building great products.
             </p>
-            <div className=" grid grid-flow-row lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 ">
-                {loading ? <loading/>  : data.map((item, index) => {return <Team_card key={index} data={item} />})}
+            <div className="lg:px-24 sm:p-5">
+
+                <div className=" grid grid-flow-row lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+                    {loading ? <loading/>  : data.map((item, index) => {return <Team_card key={index} data={item} />})}
+                </div>
             </div>
 
 
