5d10e67865b7447b1c56c6fb537484c654075a8b
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-09-19 23:55:34 +0530

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,11 +13,9 @@ function Header() {
             Google Developer Student Club
           </h1>
           <div className=" text-center">
-          <p className='font-serif font-medium text-lg'>The Google Developer's Student Club, JIS UNIVERSITY, 💐 welcomes all of you wholeheartedly. We Welcome students to grow and learn together.
-
-            We at JIS University build a <span className='underline decoration-solid decoration-cyan-300 underline-offset-2'>peer-to-peer</span>  learning process making a better environment for students to learn and develop.
-
-            This year our Club is aiming to organize resourceful <span className='underline decoration-solid decoration-violet-600 underline-offset-4'>BootCamps</span> and Interesting Workshops. The major idea is to learn new skills through hands-on workshops and create innovative projects which will help in the betterment of our <span className='underline decoration-solid decoration-yellow-300 underline-offset-2'>Society</span>.</p>
+            <p className='font-serif font-medium text-lg'>The Google Developer's Student Club, JIS UNIVERSITY, 💐 welcomes all of you wholeheartedly. We Welcome students to grow and learn together.
+              We at JIS University build a <span className='underline decoration-solid decoration-cyan-300 underline-offset-2'>peer-to-peer</span>  learning process making a better environment for students to learn and develop.
+              This year our Club is aiming to organize resourceful <span className='underline decoration-solid decoration-violet-600 underline-offset-4'>BootCamps</span> and Interesting Workshops. The major idea is to learn new skills through hands-on workshops and create innovative projects which will help in the betterment of our <span className='underline decoration-solid decoration-yellow-300 underline-offset-2'>Society</span>.</p>
 
           </div>
         </div>
