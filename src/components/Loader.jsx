9776be0cde6e85846c890a6861e36af816ee51d8
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-09-19 23:55:43 +0530

    Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loader() {
+    return (
+        <div className="flex flex-col justify-center items-center min-w-fit">
+            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-cyan-300"></div>
+        </div>
+    )
+}
+
+export default Loader
