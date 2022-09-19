e9fb803515b20c8b74bc0c1994a4bc2dc5025a99
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-09-19 23:53:45 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,19 @@ import Header from './components/Header'
 import Team from './components/Team'
 // import Loading from './components/Loading'
 import SpeedDail from './components/speedDail'
-import Events  from './components/Events'
+import Events from './components/Events'
 import Newsletter from './components/Newsletter'
 import Community from './components/Community'
 
 function App() {
 
+  // set dark mode as default
+
+
   return (
     <>
       <Hero />
-      {/* <Header /> */}
+      <Header />
       <Events />
       <Team />
       <Newsletter />
