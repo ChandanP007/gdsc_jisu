1abfc50c73078ea1884e6975f0c5ea1fd43175a9
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-09-19 23:56:26 +0530

    Contact page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div className='pt-20 text-center mx-auto dark:bg-[#182232] h-[720px]'>
-      <h1 className='lg:text-6xl text-2xl dark:text-white font-mono text-center text-black' >Comming Soon </h1>
+    <div className='pt-20 text-center mx-auto dark:bg-[#182232] h-[720px] min-h-screen '>
+      <h1 className='lg:text-6xl text-2xl dark:text-white font-mono text-center text-black' >Comming Soon : Contact</h1>
+      <img src="https://i.giphy.com/media/dLmEzHozhc9WbTkwPa/giphy.webp" className='mx-auto pt-24' />
     </div>
   )
 }
