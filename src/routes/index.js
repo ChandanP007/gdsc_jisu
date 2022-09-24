a9f6301d7ddcca6e15130d12cfb26eb8cd0fea9e
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-09-24 23:05:19 +0530

    update src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@
 export { default as Teams } from './Team';
 export { default as Projects } from './Projects';
 export { default as Contact } from './Contact';
-export { default as Timeline } from './Timeline';
+export { default as Blogs } from './Blogs';
 export { default as Events } from './Events';
 export { default as NotFound } from './NotFound';
 // export { default as Login } from './Login';
