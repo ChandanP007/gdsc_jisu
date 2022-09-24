41a72f301208013696d34886f051980e3ce24af1
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-09-24 23:05:05 +0530

    update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ function Navbar() {
 
   return (
     <>
-      <div className='lg:flex   flex-row justify-between  lg:items-center lg:justify-between p-5  shadow-black shadow-sm bg-white bg-opacity-90  dark:shadow-slate-50  sticky top-0  dark:bg-bg-dark dark:text-white z-50'>
+      <div className='lg:flex   flex-row justify-between  lg:items-center lg:justify-between px-5 py-2 shadow-black shadow-sm bg-white bg-opacity-90  dark:shadow-slate-50  sticky top-0  dark:bg-bg-dark dark:text-white z-50'>
         <div className="flex flex-row justify-between">
           <div className='inline '>
             <Link to='/'>
@@ -59,35 +59,34 @@ function Navbar() {
                 <span className='text-red-600 font-mono font-semibold text-4xl'>D</span>
                 <span className='text-yellow-600 font-mono font-semibold text-4xl'>S</span>
                 <span className='text-blue-600 font-mono font-semibold text-4xl dark:text-blue-400'>C </span>
-                <span id="jis--university" className='text-black  font-mono font-semibold text-4xl hover:underline underline-offset-4 decoration-green-500 dark:text-white'>JISU</span>
+                <span id="jis--university" className='text-black  font-mono font-semibold text-3xl hover:underline underline-offset-4 decoration-green-500 dark:text-white'>JIS University</span>
               </span>
             </Link>
           </div>
 
           <div className="">
             <span className=' lg:hidden text-3xl'>
-              {open ? <CloseIcon onClick={toogleMenu} className='cursor-pointer menu-options' /> : <SegmentIcon onClick={toogleMenu} className='cursor-pointer menu-options text-6xl' />}
+              {!open ? <CloseIcon onClick={toogleMenu} className='cursor-pointer menu-options' /> : <SegmentIcon onClick={toogleMenu} className='cursor-pointer menu-options text-6xl' />}
             </span>
           </div>
 
         </div>
 
         <div className="links dark:bg-bg-dark  lg:flex flex-row hidden" id="menu-list">
-          <ul className='lg:flex lg:flex-row lg:justify-end lg:opacity-100 opacity-100 text-center flex-col   p-10 gap-6 sm:flex   lg:py-0  lg:pl-0 '>
+          <ul className='lg:flex lg:flex-row lg:justify-end lg:opacity-100 opacity-100 text-center flex-col   p-10 gap-2 sm:flex   lg:py-0  lg:pl-0 '>
             <li><a href="#" ><Link to={'/'} className="nav-list   hover:font-semibold " >Home</Link></a></li>
             <li><a href="#" ><Link to={'/events'} className="nav-list   hover:font-semibold ">Events</Link></a></li>
-            <li><a href="#" ><Link to={'/timeline'} className="nav-list  hover:font-semibold   ">Timeline</Link></a></li>
+            <li><a href="#" ><Link to={'/blogs'} className="nav-list  hover:font-semibold   ">Blogs</Link></a></li>
             <li><a href="#" ><Link to={'/projects'} className="nav-list   hover:font-semibold " >Projects</Link></a></li>
             <li><a href="#" ><Link to={'/team'} className="nav-list  hover:font-semibold " >Teams</Link></a></li>
             <li><a href="#" ><Link to={'/contact'} className="nav-list   hover:font-semibold " >Join Us</Link></a></li>
             {/* Light Switch  */}
-            <label className="switch " >
+            <label className="switch mx-auto" >
               <input type="checkbox" id='checkbox' />
               <span className="slider round shadow-md shadow-slate-600 dark:border-cyan-300 border-opacity-100 rounded border-2" onClick={toggleTheme}></span>
             </label>
             <span className='pl-2'>{theme === 'light' ? <Brightness5Icon /> : <Brightness2Icon />}</span>
           </ul>
-
         </div>
       </div>
     </>
