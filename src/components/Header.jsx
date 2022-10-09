6695004560ef24ce31f86b30d5c4e871bee5ee45
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-10-09 20:06:30 +0530

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
             <h4 class="brandingt">Jis University</h4>
           </div>
 
-          <div className=" text-clip px-20 herop">
+          <div className=" text-clip lg:px-16 px-2 herop">
             <p className='font-mono font-medium text-lg'>The Google Developer's Student Club, <span className='community'>JIS UNIVERSITY</span> , 💐 welcomes all of you wholeheartedly. We Welcome students to grow and learn together.
               We at JIS University build a <span className='underline font-extrabold font-mono decoration-solid decoration-cyan-300 underline-offset-2'>peer-to-peer</span>  learning process making a better environment for students to learn and develop.
               This year our Club is aiming to organize resourceful <span className='underline  font-extrabold font-mono  decoration-solid decoration-violet-600 underline-offset-4'>BootCamps</span> and Interesting Workshops. The major idea is to learn new skills through hands-on workshops and create innovative projects which will help in the betterment of our <span className='underline  font-extrabold font-mono  decoration-solid decoration-yellow-300 underline-offset-2'>Society</span>.</p>
