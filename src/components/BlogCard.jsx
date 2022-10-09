f39bfc81efadc90cd42a33162ad36423d7ba924e
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-10-09 20:58:23 +0530

    update BlogCard.jsx, abbhishek-stories-copy.json and Blogs.jsx update BlogCard.jsx

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-
+import React, { lazy, Suspense } from 'react'
+import Loader from '../components/Loader.jsx'
 import ReactAudioPlayer from 'react-audio-player';
+
 
 export default function BlogCard({ blog }) {
 
@@ -10,6 +11,9 @@ export default function BlogCard({ blog }) {
 
     return (
         <>
+            <Suspense fallback={Loader}>
+
+            </Suspense>
             <div
                 onClick={
                     () => {
@@ -17,38 +21,31 @@ export default function BlogCard({ blog }) {
                     }
                 }
                 className="border-2 border-black dark:border-white rounded-md p-2 m-2 cursor-pointer">
-                <div className='flex flex-row gap-2'>
+                <div className='flex lg:flex-row flex-col gap-2'>
                     <div className='basis-1/2'>
-                        <img src={blog?.coverImage} alt={blog?.title} />
-
+                        <img src={blog.coverImage} alt={blog.slug} />
                     </div>
                     <div className='text-start basis-1/2'>
-                        <h1 className="text-black dark:text-white text-2xl font-mono font-bold  pb-2">{blog.title}</h1>
-                        <p className="text-black dark:text-white ">{blog?.brief}</p>
-
+                        <h1 className="text-black text-center dark:text-white text-2xl font-mono font-bold  pb-2">{blog.title}</h1>
+                        <hr />
+                        <p className="text-black dark:text-white ">{blog.brief}</p>
                         <div className='flex flex-col gap-2 justify-center text-center'>
                             <div >
                                 <p className="text-black dark:text-white ">Listen Blog In Audio</p>
                             </div>
                             <div className=' justify-center flex '>
                                 <ReactAudioPlayer
+                                    // src={blog?.audioUrls[0]}
                                     src={blog?.audioUrls?.male ? blog.audioUrls.male : blog.audioUrls.female}
                                     controls
-
+                                    className='w-1/2'
                                 />
-
                             </div>
                         </div>
-                        <p className="text-black dark:text-white text-start">Views : {blog?.views}</p>
-
-
+                        <p className="text-black dark:text-white text-start">Views : {blog.views}</p>
                     </div>
-
                 </div>
-
-
             </div>
-
         </>
     )
 }
--- src/routes/Blogs.jsx
+++ src/routes/Blogs.jsx
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import BlogCard from "../components/BlogCard.jsx"
+import React, { useState, useEffect, lazy, Suspense } from 'react'
 
-import Blog_Data from './../data/abbhishek-stories-copy.json'
+const { BlogCard } = lazy(() => import("../components/BlogCard.jsx"))
 
-function BlogCardLoading() {
-  return (
-    <>Loading</>
-  )
-}
-function Blogs() {
+import Blog_Data from '../data/abbhishek-stories-copy.json'
+import Loader from '../components/Loader.jsx'
+
+
+export default function Blogs() {
 
   const [blogs, setBlogs] = useState([])
   const [loading, setLoading] = useState(true)
@@ -22,36 +20,31 @@ function Blogs() {
   return (
     <>
       <div className='dark:bg-[#182232]  min-h-screen'>
-
         <div className='pt-8 text-center container mx-auto  '>
           <h1 className='lg:text-6xl text-2xl dark:text-white font-mono text-center text-black' >Blogs</h1>
           <p className='text-black dark:text-white text-center text-base'>Here are some of the blogs written by our members</p>
-
           <div className='flex flex-row justify-end'>
-
             <input type="text" placeholder="Search Your Blogs" className="border-2 border-black dark:border-white rounded-md p-2 text-center" onChange={(e) => setSearch(e.target.value)} />
-
           </div>
-
-          <div className="flex flex-col  gap-2">
-            {loading ? <BlogCardLoading /> : blogs.filter((blog) => {
-              if (search == "") {
-                return blog
-              } else if (blog.title.toLowerCase().includes(search.toLowerCase())) {
-                console.log(blog.title)
-                return blog
-              }
-            }).map((blog) => {
-              return (
-                <BlogCard blog={blog} />
-
-              )
-            })}
-          </div>
+          <Suspense fallback={<Loader />}>
+            <div className="flex flex-col  gap-2">
+              {loading ? <Loader /> : blogs.filter((blog) => {
+                if (search == "") {
+                  return blog
+                } else if (blog.title.toLowerCase().includes(search.toLowerCase())) {
+                  console.log(blog.title)
+                  return blog
+                }
+              }).map((blog) => {
+                return (
+                  <BlogCard blog={blog} />
+                )
+              })}
+            </div>
+          </Suspense>
         </div>
       </div>
     </>
   )
 }
 
-export default Blogs
